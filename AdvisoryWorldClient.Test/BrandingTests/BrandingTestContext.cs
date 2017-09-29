b87f1ad619b1065fe968dd36aebbd3f0a0be3dde
Brian Seabolt <brianmseabolt@gmail.com>
2017-09-29 11:48:00 -0400

    Updates to add Logo end point and tests to validate for Orion

--- AdvisoryWorldClient.Test/BrandingTests/BrandingTestContext.cs
+++ AdvisoryWorldClient.Test/BrandingTests/BrandingTestContext.cs
@@ -14,16 +14,16 @@ namespace AdvisoryWorldClient.Test.BrandingTests
         public async void ShouldUpdatePrivateLabelDetails()
         {
             var privateLabel = new PrivateLabel() {};
-            privateLabel.UserId = 25772;
+            privateLabel.UserId = 27414;
 
-            privateLabel.Colors.Theme.Color1 = "#650000";
-            privateLabel.Colors.Theme.Color2 = "#00E500";
-            privateLabel.Colors.Theme.Color3 = "#0000E5";
-            privateLabel.Colors.Theme.Color4 = "#000000";
-            privateLabel.Colors.Theme.Color5 = "#FFFFFF";
+            privateLabel.Colors.Theme.Color1 = "#00E500";
+            //privateLabel.Colors.Theme.Color2 = "#00E500";
+            //privateLabel.Colors.Theme.Color3 = "#0000E5";
+            //privateLabel.Colors.Theme.Color4 = "#000000";
+            //privateLabel.Colors.Theme.Color5 = "#FFFFFF";
 
 
-            privateLabel.Info.AboutCompany = "testing some stuff!";
+            privateLabel.Info.AboutCompany = $"FTJ Testing for AW private label end point, called {System.DateTime.Now}!";
             privateLabel.Info.Thumbnail = File.ReadAllBytes(@"Images\What_business_to_start_2013_crop.jpg");
             privateLabel.Info.UserImage = File.ReadAllBytes(@"Images\startup.jpg");
 
